5669ab46d2e0d83070d7456d9931ba8f6daef207
Keegan White <keegan28white@gmail.com>
2023-08-04 11:27:50 -0500

    initial commit

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterfaceExercise
 {
@@ -39,6 +40,19 @@ namespace InterfaceExercise
 
             //Now, create objects of your 3 classes and give their members values;
             //Creatively display and organize their values
+
+            var car = new Car();
+
+            var truck = new Truck();
+            var vehicles = new List<IVehicle>() { car, truck };
+            
+            foreach(var vehicle in vehicles )
+            {
+                vehicle.Drive();
+                vehicle.ChangeGears(true);
+                vehicle.Reverse();
+            }
+            
         }
     }
 }
